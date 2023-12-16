9409f9cecf34a25f1e74d882b484fb787cc77dcd
racoelhosilva <racoelhosilva@gmail.com>
2023-12-16 20:27:41 +0000

    Solved problem 1 : Two Sum

---
+++ Arrays & Hashing/Easy/1_TwoSum.cpp
@@ -0,0 +1,56 @@
+/* 
+ * Problem: 1 
+ * Name: Two Sum
+ * Difficulty: Easy
+ * Topic: Arrays & Hashing
+ * Link: https://leetcode.com/problems/
+ */
+ 
+#include <bits/stdc++.h>
+using namespace std;
+
+//      Brute-Force Approach
+// Time Complexity: O(n^2)
+// Space Complexity: O(1)
+vector<int> twoSumBruteForce(vector<int>& nums, int target) {
+    for (int i = 0; i < nums.size(); i++){
+        for (int j = i+1; j < nums.size(); j++){
+            if (nums[i] + nums[j] == target){
+                return {i, j};
+            }
+        }
+    }
+    return vector<int>();
+}
+
+//      Two-Pass Hash Table
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+vector<int> twoSumTwoPassHashTable(vector<int>& nums, int target) {
+    map<int, int> valueIndex;
+    for (int i = 0; i < nums.size(); i++){
+        valueIndex[nums[i]] = i;
+    }
+    for (int i = 0; i < nums.size(); i++){
+        int complement = target - nums[i];
+        if (valueIndex.count(complement) == 1 && valueIndex[complement] != i){
+            return {i, valueIndex[complement]};
+        }
+    }
+    return vector<int>();
+}
+
+//      One-Pass Hash Table
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+vector<int> twoSumOnePassHashTable(vector<int>& nums, int target) {
+    map<int, int> valueIndex;
+    for (int i = 0; i < nums.size(); i++){
+        int complement = target - nums[i];
+        if (valueIndex.count(complement)){
+            return {i, valueIndex[complement]};
+        }
+        valueIndex[nums[i]] = i;
+    }
+    return vector<int>();
+}
